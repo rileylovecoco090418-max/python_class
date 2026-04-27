a579c46f608151056e55cf66cf8344cec12062cf
Riley <byunmk09@gmail.com>
2026-04-26 20:35:41 -0400

    add basic syntax file

---
+++ 01_basicsyntax.py
@@ -0,0 +1,29 @@
+"""
+1. Comments
+2. Importance of the indent
+"""
+
+# This is a comment.
+"""
+This is a comment
+written in 
+more than just one line
+"""
+'''
+This is another comment
+written in 
+more than just one line
+'''
+print("Hello, World!")
+
+
+# The indent is important with python
+if 5 > 2: # True
+  print("Five is greater than two!")
+
+if 5 < 2: # False
+  print("False: This would not be printed because it's false")
+
+# # This lines have an error
+# if 5 > 2:
+# print("Five is greater than two!")
